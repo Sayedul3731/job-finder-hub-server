8b0bd5fc109871741d7815e47331a6a1b7f8fdf4
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-06 00:07:44 -0800

    New Job info store in the mongodb database

--- index.js
+++ index.js
@@ -21,10 +21,20 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const jobsCollection = client.db('jobDB').collection('jobs')
+
+    app.post('/addAJob', async(req, res) => {
+        const newJob = req.body;
+        const result = await jobsCollection.insertOne(newJob)
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
