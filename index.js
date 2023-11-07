0c220e9abc43c81fefa4acd67b73094cd69b71ce
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-07 13:45:45 -0800

    When a user update a job info and click on the 'UPDATE JOB' button in the client side 'update' route then the updated job info store in the database

--- index.js
+++ index.js
@@ -31,31 +31,66 @@ async function run() {
     const jobsCollection = client.db('jobDB').collection('jobs')
     const appliedJobsCollection = client.db('jobDB').collection('appliedJobs')
 
-    app.post('/addAJob', async(req, res) => {
-        const newJob = req.body;
-        const result = await jobsCollection.insertOne(newJob)
-        res.send(result)
+    app.post('/addAJob', async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob)
+      res.send(result)
     })
 
-    app.get('/allJobs', async(req, res) => {
+    app.get('/allJobs', async (req, res) => {
       const user = req.query;
       console.log(user.email);
       let query = {}
-      if(user?.email){
+      if (user?.email) {
         query.email = user?.email
       }
       console.log(query);
       const result = await jobsCollection.find(query).toArray()
       res.send(result)
     })
-    app.get('/allJobs/:id', async(req, res) => {
+    app.get('/allJobs/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await jobsCollection.findOne(query)
       res.send(result)
     })
+    app.get('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
+    app.patch('/update/:id', async (req, res) => {
 
-    app.post('/allJobs/:id', async(req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const updateJob = req.body;
+        console.log(filter, updateJob);
+        const update = {
+          $set: {
+            name:updateJob.name,
+            email:updateJob.email,
+            category:updateJob.category,
+            title:updateJob.title,
+            salary:updateJob.salary,
+            description:updateJob.description,
+            jobPostingDate:updateJob.jobPostingDate,
+            deadline:updateJob.deadline,
+            applicants:updateJob.applicants,
+            logo:updateJob.logo,
+            photo:updateJob.photo
+          }
+        }
+        const result = await jobsCollection.updateOne(filter, update)
+        res.send(result)
+      }
+      catch {
+        console.error(error)
+      }
+    })
+
+    app.post('/allJobs/:id', async (req, res) => {
       const appliedJob = req.body;
       const result = await appliedJobsCollection.insertOne(appliedJob)
       res.send(result)
