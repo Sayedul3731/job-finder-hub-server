f161693ab31a1246933f930b938ecfd349dc0fb2
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-08 13:17:03 -0800

    token verify functionality added

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 var jwt = require('jsonwebtoken');
+var cookieParser = require('cookie-parser')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
 // middleware 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dvnw110.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,7 +27,21 @@ const client = new MongoClient(uri, {
   }
 });
 
-
+const verifyToken = async(req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('token in the middleware', token);
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'unauthorized'})
+    }
+    console.log('value in the token ', decoded);
+    req.user = decoded;
+    next()
+  })
+}
 
 async function run() {
   try {
@@ -32,12 +51,16 @@ async function run() {
     const jobsCollection = client.db('jobDB').collection('jobs')
     const appliedJobsCollection = client.db('jobDB').collection('appliedJobs')
 
-    app.post('/jwt', async (req, res) => {
+    app.post('/jwt',verifyToken, async(req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-      console.log('token', token);
-      res.send({success : true})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+      .send({success: true})
     })
     
 
@@ -57,9 +80,10 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/appliedJobs', async (req, res) => {
+    app.get('/appliedJobs',verifyToken, async (req, res) => {
       const user = req.query;
       console.log(user);
+      console.log('user in the valid token', req.user);
       const result = await appliedJobsCollection.find(user).toArray()
       res.send(result)
     })
